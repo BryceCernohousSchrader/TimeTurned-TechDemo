7e5c1a128c4be21f06ccadd655d2b1311415f48c
Jack Dekko <jdekko1@gmail.com>
2022-06-09 21:10:08 -0500

    Implemented item pickup prototype

--- Assets/Scripts/Item/ItemObjectPickup.cs
+++ Assets/Scripts/Item/ItemObjectPickup.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 // Original Author - Jack Dekko
 
+/// <summary>
+/// Player behavior that allows for "picking up" an item object
+/// Pre Conditions: ~~~~~ (if applicable)
+/// Post Conditions: ~~~~~ (if applicable)
+/// </summary>
 public class ItemObjectPickup : MonoBehaviour
 {
 
-    private bool detectedPickup = false;
     private bool pickupItem = false;
 
-    // Update is called once per frame
+    // Might be helpful to know which objects on scene are detected as pickups
+    [SerializeField] private bool detectedPickup = false;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && detectedPickup is true) {
-                Debug.Log("Pickup Item");
+                Debug.Log("Item object made child to Player.");
                 pickupItem = true;
             }
     }
@@ -24,7 +30,7 @@ public class ItemObjectPickup : MonoBehaviour
         if (collider.gameObject.tag == "Item")
         {
             //If item is detected, pickup is true otherwise false
-            Debug.Log("Item Collision");
+            Debug.Log("Item Trigger.");
             detectedPickup = true;
         }
 
@@ -32,6 +38,7 @@ public class ItemObjectPickup : MonoBehaviour
 
     void OnTriggerStay2D(Collider2D collider) 
     {
+        // Make Item object a child of the player in the hierarchy.
         if (pickupItem is true) {
                 collider.transform.parent = transform;
             }
